1759e07697be55ac1e80a451d2a86e23c33a5b3e
rémi  <remi.robert@epitech.eu>
2016-10-26 17:54:53 +0200

    Update JPUtils.m

--- JPUtils.m
+++ JPUtils.m
@@ -9,30 +9,9 @@
 
 @implementation JPUtils
 
--(id)init{
-    self = [super init];
-    if (self){
-        // do something
-    }
-    return self;
-}
-
-+(JPUtils *)singleton{
-    static JPUtils *singleton;
-    
-    if (singleton == nil){
-        singleton = [[JPUtils alloc] init];
-    }
-    return singleton;
-}
-
 #pragma mark - CHECK EMAIL
 
 +(BOOL)checkStringIsEmail:(NSString *)string{
-    return [[JPUtils singleton] checkStringIsEmail:string];
-}
-
--(BOOL)checkStringIsEmail:(NSString *)string{
     BOOL stricterFilter = YES;
     NSString *stricterFilterString = @"[A-Z0-9a-z\\._%+-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}";
     NSString *laxString = @".@([A-Za-z0-9]+\\.)+[A-Za-z]{2}[A-Za-z]*";
@@ -44,29 +23,17 @@
 #pragma mark - PREPARE IMAGE TO UPLOAD ON A WEBSERVICE
 
 +(NSString *)prepareImageToUpload:(UIImage *)image{
-    return [[JPUtils singleton] prepareImageToUpload:image];
-}
-
--(NSString *)prepareImageToUpload:(UIImage *)image{
     NSData *imageData = UIImageJPEGRepresentation(image, 0.8f);
-    NSString *imageString = [imageData base64Encoding];
+    NSString *imageString = [imageData base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
     return imageString;
 }
 
 #pragma mark - CREATE AN ALERT VIEW QUICKLY AND EASLY
 
-+(void)createAlertViewWithTitle:(NSString *)title andMessage:(NSString *)message andButton:(NSString *)button{
-    [[JPUtils singleton] createAlertViewWithTitle:title andMessage:message andButton:button];
-}
-
--(void)createAlertViewWithTitle:(NSString *)title andMessage:(NSString *)message andButton:(NSString *)button{
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title
-                                                    message:message
-                                                   delegate:self
-                                          cancelButtonTitle:button
-                                          otherButtonTitles:nil,
-                          nil];
-    [alert show];
++(void)createAlertViewWithTitle:(UIViewController *)parentController :(NSString *)title andMessage:(NSString *)message andButton:(NSString *)button{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:[UIAlertAction actionWithTitle:button style:UIAlertActionStyleCancel handler:nil]];
+    [parentController presentViewController:alertController animated:true completion:nil];
 }
 
 @end
